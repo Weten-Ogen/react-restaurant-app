94a339cfeda1bae1880351107f57a10783bf3e9e
Weten-Ogen <alphaoware@gmail.com>
2023-03-30 16:49:32 +0000

    styled the headlineCard responsively

--- src/components/HeadlineCard.js
+++ src/components/HeadlineCard.js
@@ -4,10 +4,10 @@ const HeadlineCard = ({para,amount,image}) => {
   return (
     <div className='rounded-lg relative '>
         {/*Overlay */}
-        <div className='absolute rounded-lg w-full h-full text-white text-2xl  bg-black/30 px-4 '>
+        <div className='absolute rounded-lg w-full h-full text-white text-2xl  bg-black/40 px-4 '>
             <p className='capitalize pt-1 px-2 text-slate-100'>{para}</p>
             <p className='capitalize pt-1 px-2 text-slate-300'>{amount}</p>
-            <button className='absolute bottom-0 px-4 py-2 bg-white text-black rounded-md mb-4  capitalize'>order now</button>
+            <button className='absolute bottom-0 px-4 py-2 bg-black/50 text-orange-500 hover:ring hover:ring-orange-500/20  rounded-md mb-4 hover:bg-black/40   capitalize'>order now</button>
         </div>
         <img className='w-full h-full object-cover rounded-lg max-h-[160px] md:max-h-[200px]' src={image} alt={image}/>
     </div>
